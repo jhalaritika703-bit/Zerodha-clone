9c93174647465d4a23cd3c62d6729ca29a523c58
Ritika Jhala <jhalaritika703@gmail.com>
2026-07-24 15:44:47 +0530

    Fix ESLint errors for Vercel deployment

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -6,73 +6,97 @@ function Footer() {
       <div className="container border-top mt-5">
         <div className="row mt-5">
           <div className="col">
-            <img src="media/images/logo.svg" style={{ width: "50%" }} />
+            <img
+              src="media/images/logo.svg"
+              alt="Zerodha Logo"
+              style={{ width: "50%" }}
+            />
             <p>
               &copy; 2010 - 2024, Not Zerodha Broking Ltd. All rights reserved.
             </p>
           </div>
+
           <div className="col">
             <p>Company</p>
-            <a href="">About</a>
+
+            <a href="/about">About</a>
             <br />
-            <a href="">Products</a>
+
+            <a href="/products">Products</a>
             <br />
-            <a href="">Pricing</a>
+
+            <a href="/pricing">Pricing</a>
             <br />
-            <a href="">Referral programme</a>
+
+            <a href="/">Referral programme</a>
             <br />
-            <a href="">Careers</a>
+
+            <a href="/">Careers</a>
             <br />
-            <a href="">Zerodha.tech</a>
+
+            <a href="/">Zerodha.tech</a>
             <br />
-            <a href="">Press & media</a>
+
+            <a href="/">Press & media</a>
             <br />
-            <a href="">Zerodha cares (CSR)</a>
+
+            <a href="/">Zerodha cares (CSR)</a>
             <br />
           </div>
+
           <div className="col">
             <p>Support</p>
-            <a href="">Contact</a>
+
+            <a href="/">Contact</a>
             <br />
-            <a href="">Support portal</a>
+
+            <a href="/">Support portal</a>
             <br />
-            <a href="">Z-Connect blog</a>
+
+            <a href="/">Z-Connect blog</a>
             <br />
-            <a href="">List of charges</a>
+
+            <a href="/">List of charges</a>
             <br />
-            <a href="">Downloads & resources</a>
+
+            <a href="/">Downloads & resources</a>
             <br />
           </div>
+
           <div className="col">
             <p>Account</p>
-            <a href="">Open an account</a>
+
+            <a href="/">Open an account</a>
             <br />
-            <a href="">Fund transfer</a>
+
+            <a href="/">Fund transfer</a>
             <br />
-            <a href="">60 day challenge</a>
+
+            <a href="/">60 day challenge</a>
             <br />
           </div>
         </div>
+
         <div className="mt-5 text-muted" style={{ fontSize: "14px" }}>
           <p>
-            Zerodha Broking Ltd.: Member of NSE​ &​ BSE – SEBI Registration no.:
-            INZ000031633 CDSL: Depository services through Zerodha Securities
-            Pvt. Ltd. – SEBI Registration no.: IN-DP-100-2015 Commodity Trading
-            through Zerodha Commodities Pvt. Ltd. MCX: 46025 – SEBI Registration
-            no.: INZ000038238 Registered Address: Zerodha Broking Ltd.,
-            #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School,
-            J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any
-            complaints pertaining to securities broking please write to
-            complaints@zerodha.com, for DP related to dp@zerodha.com. Please
-            ensure you carefully read the Risk Disclosure Document as prescribed
-            by SEBI | ICF
+            Zerodha Broking Ltd.: Member of NSE &amp; BSE – SEBI Registration
+            no.: INZ000031633 CDSL: Depository services through Zerodha
+            Securities Pvt. Ltd. – SEBI Registration no.: IN-DP-100-2015
+            Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025 –
+            SEBI Registration no.: INZ000038238 Registered Address: Zerodha
+            Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence
+            Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka,
+            India. For any complaints pertaining to securities broking please
+            write to complaints@zerodha.com, for DP related to dp@zerodha.com.
+            Please ensure you carefully read the Risk Disclosure Document as
+            prescribed by SEBI | ICF.
           </p>
 
           <p>
             Procedure to file a complaint on SEBI SCORES: Register on SCORES
             portal. Mandatory details for filing complaints on SCORES: Name,
             PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
-            Communication, Speedy redressal of the grievances
+            Communication, Speedy redressal of the grievances.
           </p>
 
           <p>
--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -4,7 +4,7 @@ function Team() {
   return (
     <div className="container">
       <div className="row p-3 mt-5 border-top">
-        <h1 className="text-center ">People</h1>
+        <h1 className="text-center">People</h1>
       </div>
 
       <div
@@ -14,25 +14,32 @@ function Team() {
         <div className="col-6 p-3 text-center">
           <img
             src="media/images/nithinKamath.jpg"
+            alt="Nithin Kamath"
             style={{ borderRadius: "100%", width: "50%" }}
           />
           <h4 className="mt-5">Nithin Kamath</h4>
           <h6>Founder, CEO</h6>
         </div>
+
         <div className="col-6 p-3">
           <p>
             Nithin bootstrapped and founded Zerodha in 2010 to overcome the
             hurdles he faced during his decade long stint as a trader. Today,
             Zerodha has changed the landscape of the Indian broking industry.
           </p>
+
           <p>
             He is a member of the SEBI Secondary Market Advisory Committee
             (SMAC) and the Market Data Advisory Committee (MDAC).
           </p>
+
           <p>Playing basketball is his zen.</p>
+
           <p>
-            Connect on <a href="">Homepage</a> / <a href="">TradingQnA</a> /{" "}
-            <a href="">Twitter</a>
+            Connect on{" "}
+            <a href="https://zerodha.com/">Homepage</a> /{" "}
+            <a href="https://tradingqna.com/">TradingQnA</a> /{" "}
+            <a href="https://x.com/Nithin0dha">Twitter</a>
           </p>
         </div>
       </div>
